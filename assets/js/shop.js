b6e156faa678fded19dfb827d4c6b7a6617a1d37
Jake <68775263+jakedothow@users.noreply.github.com>
2022-05-05 22:51:20 +0200

    Hopefully final :')

---
+++ assets/js/shop.js
@@ -0,0 +1,44 @@
+var productData = [];
+
+console.log("fetching catalog");
+fetch('https://roc-api.jake.how/shop/catalog')
+    .then(response => response.json())
+    .then(data => productData = data)
+    .finally(() => populateShop());
+
+function populateShop() {
+    var template = document.querySelector("#product-template").innerHTML;
+    var templateScript = Handlebars.compile(template);
+
+    console.log(productData);
+    if(productData.length == 0) {
+        return;
+    }
+
+    // Remove the error message that is displayed in place of an empty catalog
+    document.querySelector(".catalog-empty").remove();
+
+    productData.forEach(product => {
+        // Hackily set the icon manually, I'm so good
+        product.icon = window.__SHOPPING_CART__.items.includes(product.id) ? "fa-circle-check" : "fa-cart-plus";
+
+        var html = templateScript(product);
+        var element = document.createElement("div");
+        element.innerHTML = html;
+
+        document.querySelector(".products").appendChild(element);
+    });
+}
+
+function toggleCart(button, itemId) {
+    if(window.__SHOPPING_CART__.items.includes(itemId)) {
+        window.__SHOPPING_CART__.removeItem(itemId);
+        button.firstChild.classList.remove("fa-circle-check");
+        button.firstChild.classList.add("fa-cart-plus");
+    }
+    else {
+        window.__SHOPPING_CART__.addItem(itemId);
+        button.firstChild.classList.remove("fa-cart-plus");
+        button.firstChild.classList.add("fa-circle-check");
+    }
+}
---
+++ assets/js/shopping-cart.js
@@ -0,0 +1,34 @@
+window.__SHOPPING_CART__ = {
+    items: [],
+    addItem: function(itemId) {
+        this.items.push(itemId);
+        this.refreshCartButton();
+        console.log(`added ${itemId} to shopping cart`);
+    },
+    removeItem: function(itemId) {
+        this.items.splice(this.items.indexOf(itemId), 1);
+        this.refreshCartButton();
+        console.log(`removed ${itemId} to shopping cart`);
+    },
+    refreshCartButton: function() {
+        document.querySelector("#cart-count").innerHTML = this.items.length;
+    }
+}
+
+var cartData = getCookie("CART");
+if(cartData === undefined) {
+    console.log("cart data is undefined, either first time visitor or user is accessing locally which disables cookies.");
+    document.cookie = "CART=['catnip-bellenblaas']";
+    window.__SHOPPING_CART__.items.push("catnip-bellenblaas");
+}
+else {
+    window.__SHOPPING_CART__.items = JSON.parse(cartData);
+    window.__SHOPPING_CART__.refreshCartButton();
+}
+
+function getCookie(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) {
+        return match[2];
+    }
+}
